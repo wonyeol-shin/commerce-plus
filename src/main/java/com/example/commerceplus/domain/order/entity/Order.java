307836ff0d8d10c3e50f0de5f5dc3266248e6dda
wonyeolShin <lwy171400@gmail.com>
2026-09-13 13:51:04 +0900

    [feature/order] Order 엔티티 만료주문 취소 메서드 추가

--- src/main/java/com/example/commerceplus/domain/order/entity/Order.java
+++ src/main/java/com/example/commerceplus/domain/order/entity/Order.java
@@ -84,8 +84,17 @@ public class Order extends BaseTimeEntity {
         return firstName + " 외 " + (orderItems.size() - 1) + "건";
     }
 
-    // 취소 가능한 상태 검증
+    // 취소 가능한 상태 검증(결제 후 취소)
     public void cancel() {
+        if (this.status != OrderStatus.PAYMENT_PENDING) {
+            throw new BusinessException(ErrorCode.INVALID_ORDER_STATUS);
+        }
+
+        this.status = OrderStatus.CANCELED;
+    }
+
+    // 취소 가능한 상태 검증(결제전 시간만료로 인한 취소)
+    public void cancelByTimeout() {
         if (this.status != OrderStatus.PAYMENT_PENDING) {
             throw new BusinessException(ErrorCode.INVALID_ORDER_STATUS);
         }
@@ -98,7 +107,6 @@ public class Order extends BaseTimeEntity {
             throw new BusinessException(ErrorCode.ORDER_ACCESS_DENIED);
         }
     }
-
 
     // 결제 부분 추가
     public void validatePaymentAmount(int requestAmount) {
