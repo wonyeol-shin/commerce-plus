6d9d4eeb6a8cbc110706af75da7bf7927420a2c6
wonyeolShin <lwy171400@gmail.com>
2026-09-13 13:50:13 +0900

    [feature/common] 스케줄링 활성화

--- src/main/java/com/example/commerceplus/CommerceplusApplication.java
+++ src/main/java/com/example/commerceplus/CommerceplusApplication.java
@@ -3,11 +3,13 @@ package com.example.commerceplus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableCaching
 @EnableMethodSecurity
+@EnableScheduling
 public class CommerceplusApplication {
 
     public static void main(String[] args) {
